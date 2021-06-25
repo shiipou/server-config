624f6732e58f79e003fe6e26504aa4a1225300cc
Flavien CADET <shiishii@nocturlab.fr>
2021-06-25 18:10:00 +0200

    Install Proxmox

---
+++ proxmox.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo -s
+
+echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+
+wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg  # optional, if you have a non-default umask
+
+apt update && apt full-upgrade -y
+
+apt install proxmox-ve postfix open-iscsi
+
+apt remove os-prober
