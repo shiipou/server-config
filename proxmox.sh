a4a4d6a3b28afc45706c8fcc357f2d9779d18a4d
Flavien CADET <shiishii@nocturlab.fr>
2021-06-25 18:11:13 +0200

    Install docker

--- proxmox.sh
+++ proxmox.sh
@@ -2,6 +2,7 @@
 
 sudo -s
 
+# Install Proxmox
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
@@ -12,3 +13,7 @@ apt update && apt full-upgrade -y
 apt install proxmox-ve postfix open-iscsi
 
 apt remove os-prober
+
+# Install docker
+curl -sSL https://get.docker.com | bash
+usermod -aG docker $USER
