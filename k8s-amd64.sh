462bcf0f28897acb7947e2a1b595e9eb8bb4ee9a
Flavien CADET <shiishii@nocturlab.fr>
2021-07-10 21:35:44 +0200

    Create k8s-amd64.sh

---
+++ k8s-amd64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Missing dep
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+
+# Add k8s repo and signing key
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+# Isntalling k8s
+apt-get update
+apt-get install -y kubectl
