968abf094250e464ab505b12f7e098391e78b5d4
Flavien CADET <shiishii@nocturlab.fr>
2021-07-10 22:46:14 +0200

    Update k8s-amd64.sh

--- k8s-amd64.sh
+++ k8s-amd64.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-# Missing dep
+# Missing dep for downloading things with curl
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
+
+
+# Enabling rooting for wireguard VPN
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
+# Installing wireguard
+apt-get install -y wireguard
+
+# Generate private and public key
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
+
+echo "## Set Up WireGuard VPN on Debian By Editing/Creating wg0.conf File ##
+[Interface]
+## My VPN server private IP address ##
+Address = 10.0.0.1/16
+
+## My VPN server port ##
+ListenPort = 51194
+
+## VPN server's private key i.e. /etc/wireguard/privatekey ##
+PrivateKey = qNwn/g+DPWgvKv4b54K3TmokV1YjrSWsIA0yXrseW1w=
+
+## Save and update this config file when a new peer (vpn client) added ##
+SaveConfig = true
+
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+" > /etc/wireguard/wg0.conf
+
 
 # Add k8s repo and signing key
 curl -fsSLo /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -22,7 +52,9 @@ kubernetesVersion: stable
 apiServer:
   certSANs:
   - "nocturlab.fr"
-controlPlaneEndpoint: "nocturlab.fr:6443"' > kubeadm-config.yaml
+controlPlaneEndpoint: "nocturlab.fr:6443"
+networking:
+  podSubnet: 192.168.0.0/16' > kubeadm-config.yaml
 
 # Init the cluster first master
-kubeadm init --pod-network-cidr=192.168.0.0/16 --config=kubeadm-config.yaml
+kubeadm init --config=kubeadm-config.yaml
