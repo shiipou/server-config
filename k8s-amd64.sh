c31d29039c66966f258049ad3ad7f62c5ae826c7
Flavien CADET <shiishii@nocturlab.fr>
2021-07-11 17:35:18 +0200

    Update k8s-amd64.sh

--- k8s-amd64.sh
+++ k8s-amd64.sh
@@ -12,7 +12,7 @@ sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 sysctl -w net.bridge.bridge-nf-call-ip6tables=1
 sysctl -w net.bridge.bridge-nf-call-iptables=1
-sysctl -w net.ipv4.conf.all.rp_filter
+sysctl -w net.ipv4.conf.all.rp_filter=1
 
 sudo sysctl --system
 
